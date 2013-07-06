f70921fe54feff3c58bad758eadd3ad58720aedb
Ubuntu <ubuntu@ip-172-31-43-236.us-west-2.compute.internal>
2013-07-06 17:18:19 +0000

    another tweak

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer(fs.readFileSync("index.html"));
+  var buffer = new Buffer((fs.readFileSync("index.html")),"utf8");
   response.send(buffer.toString("utf8",buffer));
 });
 
