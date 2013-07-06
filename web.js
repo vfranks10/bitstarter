101567518a00357f88d8d7b97db692cc972dd32d
Ubuntu <ubuntu@ip-172-31-43-236.us-west-2.compute.internal>
2013-07-06 23:15:52 +0000

    remove variable from buffertoString

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer((fs.readFileSync("index.html")),"utf8");
-  response.send(buffer.toString("utf8",buffer));
+response.send(buffer.toString("utf8"));
 });
 
-var port = process.env.PORT || 9321;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
 console.log("Listening on " + port);
 });
